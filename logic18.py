4aff60b4961a2d44b1607bdd222925a084d22e71
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-16 23:21:45 +0500

    logic18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,20 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x = a%10 #4
+    a = a//10 
+    z = a%10 #3
+    a = a//10
+    b = a%10#2
+    a = a//10
+    f = a%10#1
+    a = a//10#0   #1 2 3 4 5
+    #  x,z,b,f,g  #0 1 2 3 4
+
+    k = 0 #counter
+    if x<z and z<b and b<f and f<a:
+        k += 1
+        return True
+    if k==0:
+        return False
+print(main(76943))
