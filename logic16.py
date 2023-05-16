a6c0509edf3953d7a11e5aaaae1b1f1efbf8de49
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-16 23:18:15 +0500

    logic16

--- logic16.py
+++ logic16.py
@@ -5,5 +5,17 @@ def main(a):
         a(int): parameter a
     Returns:
         bool: answer
+
     """
-    return
+    # 13425
+    # x = a%10
+    # y = a//10
+    # z = y%10
+    # e = y//10
+    # b = e%10
+    # d = e//10
+    # f = d%10
+    # g = d//10
+    #  x,z,b,f,g   mnatematik aperatorlar bn ishlash
+    return 10000 <= a and a <= 99999
+print(main(13465))
